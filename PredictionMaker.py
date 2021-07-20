9e0d233ce15d96ac0e83b72bef8f3067eb11a817
mohammadmufti <87641912+mohammadmufti@users.noreply.github.com>
2021-07-19 22:31:40 -0700

    Updated ML Model w additional data Re-ran predictions w updated file. Instead of pulling just averages by tree, we also consider now when prior vessel went from x to y from start of yr.

--- PredictionMaker.py
+++ PredictionMaker.py
@@ -1,13 +1,21 @@
 import pandas as pd
-import numpy as np
 import csv
 import joblib
 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
 
 voyages = pd.read_csv('voyages.csv', parse_dates=["begin_date","end_date"])
-model = joblib.load('Voyage_Predictor.joblib')
+# Create a new Data Frame w relevent info to train model
+mod_voyages = pd.concat([voyages.drop(columns=["begin_date", "end_date", "end_port_id"])], axis=1)
+mod_voyages["Trip_id"] = mod_voyages.groupby("vessel").cumcount()+1
+
+# model = joblib.load('Voyage_Predictor.joblib')
+X = mod_voyages
+y = voyages["end_port_id"]
+model = DecisionTreeClassifier()
+model.fit(X,y) #Fit the model
+# Stores the trained model
+joblib.dump(model, 'Voyage_Predictor.joblib')
 
 vessels = pd.unique(voyages['vessel'])  # Find unique vessals
 predictions = [['vessel', 'begin_port_id', 'end_port_id', 'voyage']]  # Initialize Storage for predictions
@@ -16,12 +24,12 @@ predictions = [['vessel', 'begin_port_id', 'end_port_id', 'voyage']]  # Initiali
 for vessel in vessels:
     voyage = 1  # Initialize iterator for predictions
     begin_port = voyages.loc[voyages.where(voyages.vessel == vessel).last_valid_index(), 'end_port_id']
-    end_port = model.predict([[vessel, begin_port]])
+    end_port = model.predict([[vessel, begin_port, voyage]])
     predictions.append([vessel, begin_port, end_port[0], voyage])
     while (voyage != 3):
         begin_port = end_port[0]
         voyage += 1
-        end_port = model.predict([[vessel, begin_port]])
+        end_port = model.predict([[vessel, begin_port, voyage]])
         predictions.append([vessel, begin_port, end_port[0], voyage])
 
 # Store Predictions to csv
